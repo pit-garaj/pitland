9bec8f733902b66c6b8393943be11b9f0b0c6689
fidget <fidget@fidget.ru>
2023-01-10 22:51:53 +0300

    feature(catalog): Убирает цены из быстрого поиска если нет в наличии

--- www/bitrix/templates/aspro_next/components/bitrix/search.title/corp/ajax.php
+++ www/bitrix/templates/aspro_next/components/bitrix/search.title/corp/ajax.php
@@ -1,10 +1,13 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+<?php if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) {
+    die();
+}
 
+/** @var array $arResult */
 
 CModule::IncludeModule( 'iblock' );
 
-if ( $arResult['query'] )
-{
+
+if ($arResult['query'] ) {
 	$arFilter = Array(
 		'IBLOCK_ID' => 23,
 		'ACTIVE' => 'Y',
@@ -20,23 +23,22 @@ if ( $arResult['query'] )
 }
 
 
-
-if (empty($arResult["CATEGORIES"]) && !isset( $arSection )) return;?>
+if (empty($arResult["CATEGORIES"]) && !isset( $arSection )) {
+    return;
+}
+?>
 <div class="bx_searche scrollbar">
 
-	<?
-	if ( $arSection['NAME'] )
-	{
-		?>
+	<?php if ($arSection['NAME']) { ?>
 			<a class="bx_item_block others_result" href="<?=$arSection["SECTION_PAGE_URL"]?>">
 				<div class="maxwidth-theme">
 					<div class="bx_img_element">
-						<?if($arSection["PICTURE"]):?>
-							<?
+						<?php if($arSection["PICTURE"]): ?>
+							<?php
 							$arSection["PICTURE"] = CFile::ResizeImageGet( $arSection["PICTURE"], Array( 'width' => 80, 'height' => 80 ) );
 							?>
 							<img src="<?=$arSection["PICTURE"]['src']?>">
-						<?endif;?>
+						<?php endif; ?>
 					</div>
 					<div class="bx_item_element">
 						<span><?=$arSection["NAME"]?></span>
@@ -44,14 +46,11 @@ if (empty($arResult["CATEGORIES"]) && !isset( $arSection )) return;?>
 					<div style="clear:both;"></div>
 				</div>
 			</a>
-		<?
-	}
-	?>
+  <?php } ?>
 
-	<?foreach($arResult["CATEGORIES"] as $category_id => $arCategory):?>
-		<?foreach($arCategory["ITEMS"] as $i => $arItem):?>
-			<?//=$arCategory["TITLE"]?>
-			<?if($category_id === "all"):?>
+	<?php foreach($arResult["CATEGORIES"] as $category_id => $arCategory): ?>
+		<?php foreach($arCategory["ITEMS"] as $i => $arItem): ?>
+			<?php if($category_id === "all"): ?>
 				<div class="bx_item_block all_result">
 					<div class="maxwidth-theme">
 						<div class="bx_item_element">
@@ -60,55 +59,57 @@ if (empty($arResult["CATEGORIES"]) && !isset( $arSection )) return;?>
 						<div style="clear:both;"></div>
 					</div>
 				</div>
-			<?elseif(isset($arResult["ELEMENTS"][$arItem["ITEM_ID"]])):
+			<?php elseif(isset($arResult["ELEMENTS"][$arItem["ITEM_ID"]])):
 				$arElement = $arResult["ELEMENTS"][$arItem["ITEM_ID"]];?>
 				<a class="bx_item_block" href="<?=$arItem["URL"]?>">
 					<div class="maxwidth-theme">
 						<div class="bx_img_element">
-							<?if(is_array($arElement["PICTURE"])):?>
+							<?php if(is_array($arElement["PICTURE"])): ?>
 								<img src="<?=$arElement["PICTURE"]["src"]?>">
-							<?else:?>
+							<?php else: ?>
 								<img src="<?=SITE_TEMPLATE_PATH?>/images/no_photo_small.png" width="38" height="38">
-							<?endif;?>
+							<?php endif; ?>
 						</div>
 						<div class="bx_item_element">
 							<span><?=$arItem["NAME"]?></span>
+              <?php if ($arElement['CATALOG_AVAILABLE'] === 'Y'): ?>
 							<div class="price cost prices">
 								<div class="title-search-price">
-									<?if($arElement["MIN_PRICE"]){?>
-										<?if($arElement["MIN_PRICE"]["DISCOUNT_VALUE"] < $arElement["MIN_PRICE"]["VALUE"]):?>
+									<?php if ($arElement["MIN_PRICE"]) { ?>
+										<?php if ($arElement["MIN_PRICE"]["DISCOUNT_VALUE"] < $arElement["MIN_PRICE"]["VALUE"]): ?>
 											<div class="price"><?=$arElement["MIN_PRICE"]["PRINT_DISCOUNT_VALUE"]?></div>
 											<div class="price discount">
 												<strike><?=$arElement["MIN_PRICE"]["PRINT_VALUE"]?></strike>
 											</div>
-										<?else:?>
+										<?php else: ?>
 											<div class="price"><?=$arElement["MIN_PRICE"]["PRINT_VALUE"]?></div>
-										<?endif;?>
-									<?}else{?>
-										<?foreach($arElement["PRICES"] as $code=>$arPrice):?>
-											<?if($arPrice["CAN_ACCESS"]):?>
-												<?if (count($arElement["PRICES"])>1):?>
+										<?php endif; ?>
+									<?php } else { ?>
+										<?php foreach($arElement["PRICES"] as $code=>$arPrice): ?>
+											<?php if ($arPrice["CAN_ACCESS"]):?>
+												<?php if (count($arElement["PRICES"]) > 1): ?>
 													<div class="price_name"><?=$arResult["PRICES"][$code]["TITLE"];?></div>
-												<?endif;?>
-												<?if($arPrice["DISCOUNT_VALUE"] < $arPrice["VALUE"]):?>
+												<?php endif; ?>
+												<?php if($arPrice["DISCOUNT_VALUE"] < $arPrice["VALUE"]): ?>
 													<div class="price"><?=$arPrice["PRINT_DISCOUNT_VALUE"]?></div>
 													<div class="price discount">
 														<strike><?=$arPrice["PRINT_VALUE"]?></strike>
 													</div>
-												<?else:?>
+												<?php else: ?>
 													<div class="price"><?=$arPrice["PRINT_VALUE"]?></div>
-												<?endif;?>
-											<?endif;?>
-										<?endforeach;?>
-									<?}?>
+												<?php endif; ?>
+											<?php endif; ?>
+										<?php endforeach; ?>
+									<?php } ?>
 								</div>
 							</div>
+              <?php endif; ?>
 						</div>
 						<div style="clear:both;"></div>
 					</div>
 				</a>
-			<?else:?>
-				<?if($arItem["MODULE_ID"]):?>
+			<?php else: ?>
+				<?php if ($arItem["MODULE_ID"]): ?>
 					<a class="bx_item_block others_result" href="<?=$arItem["URL"]?>">
 						<div class="maxwidth-theme">
 							<div class="bx_item_element">
@@ -117,8 +118,8 @@ if (empty($arResult["CATEGORIES"]) && !isset( $arSection )) return;?>
 							<div style="clear:both;"></div>
 						</div>
 					</a>
-				<?endif;?>
-			<?endif;?>
-		<?endforeach;?>
-	<?endforeach;?>
+				<?php endif; ?>
+			<?php endif; ?>
+		<?php endforeach; ?>
+	<?php endforeach; ?>
 </div>
