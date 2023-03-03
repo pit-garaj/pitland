fb4b8236b33bfa3ac0a63deca50366049ba7bd00
fidget <fidget@fidget.ru>
2023-03-04 00:40:48 +0300

    feature(project): Отключает слайдер если баннеров меньше 2-х

--- public_html/local/components/ninja/header-banner/templates/main/template.php
+++ public_html/local/components/ninja/header-banner/templates/main/template.php
@@ -16,51 +16,56 @@ $this->addExternalJS("https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js
 
 <div class="maxwidth-theme">
   <div class="header-banners-wrapper">
-    <div class="header-banners-item">
-      <div class="swiper header-banners header-banners__main<?php if(count($arResult['BANNERS']['MAIN']) > 1): ?> header-slider-main<?php endif; ?>">
-        <div class="swiper-wrapper">
-            <?php foreach($arResult['BANNERS']['MAIN'] as $banner): ?>
-              <div class="swiper-slide header-banner-item" data-swiper-autoplay="<?=$banner['autoplay']?>">
-                  <?php if ($banner['link']): ?>
-                    <a href="<?=$banner['link']?>" target="<?=$banner['target']?>"><img src="<?=$banner['picture']['src']?>" class="header-banner-item__picture" alt="" /></a>
-                  <?php else: ?>
-                    <img src="<?=$banner['picture']['src']?>" class="header-banner-item__picture" alt="" />
-                  <?php endif; ?>
-              </div>
-            <?php endforeach; ?>
+      <?php if(!empty($arResult['BANNERS']['MAIN'])): ?>
+        <div class="header-banners-item">
+          <div class="swiper header-banners header-banners__main<?php if(count($arResult['BANNERS']['MAIN']) > 1): ?> header-slider-main<?php endif; ?>">
+            <div class="swiper-wrapper">
+                <?php foreach($arResult['BANNERS']['MAIN'] as $banner): ?>
+                  <div class="swiper-slide header-banner-item" data-swiper-autoplay="<?=$banner['autoplay']?>">
+                      <?php if ($banner['link']): ?>
+                        <a href="<?=$banner['link']?>" target="<?=$banner['target']?>"><img src="<?=$banner['picture']['src']?>" class="header-banner-item__picture" alt="" /></a>
+                      <?php else: ?>
+                        <img src="<?=$banner['picture']['src']?>" class="header-banner-item__picture" alt="" />
+                      <?php endif; ?>
+                  </div>
+                <?php endforeach; ?>
+            </div>
+              <?php if(count($arResult['BANNERS']['MAIN']) > 1): ?>
+                <div class="header-banners-nav header-banners-nav__prev">
+                  <div class="header-banners-nav__icon"></div>
+                </div>
+                <div class="header-banners-nav header-banners-nav__next">
+                  <div class="header-banners-nav__icon"></div>
+                </div>
+              <?php endif; ?>
+          </div>
         </div>
-          <?php if(count($arResult['BANNERS']['MAIN']) > 1): ?>
-            <div class="header-banners-nav header-banners-nav__prev">
-              <div class="header-banners-nav__icon"></div>
+      <?php endif; ?>
+
+      <?php if(!empty($arResult['BANNERS']['ADDITIONAL'])): ?>
+        <div class="header-banners-item">
+          <div class="swiper header-banners header-banners__additional<?php if(count($arResult['BANNERS']['ADDITIONAL']) > 1): ?> header-slider-additional<?php endif; ?>">
+            <div class="swiper-wrapper">
+                <?php foreach($arResult['BANNERS']['ADDITIONAL'] as $banner): ?>
+                  <div class="swiper-slide header-banner-item" data-swiper-autoplay="<?=$banner['autoplay']?>">
+                      <?php if ($banner['link']): ?>
+                        <a href="<?=$banner['link']?>" target="<?=$banner['target']?>"><img src="<?=$banner['picture']['src']?>" class="header-banner-item__picture" alt="" /></a>
+                      <?php else: ?>
+                        <img src="<?=$banner['picture']['src']?>" class="header-banner-item__picture" alt="" />
+                      <?php endif; ?>
+                  </div>
+                <?php endforeach; ?>
             </div>
-            <div class="header-banners-nav header-banners-nav__next">
-              <div class="header-banners-nav__icon"></div>
-            </div>
-          <?php endif; ?>
-      </div>
-    </div>
-    <div class="header-banners-item">
-      <div class="swiper header-banners header-banners__additional<?php if(count($arResult['BANNERS']['ADDITIONAL']) > 1): ?> header-slider-additional<?php endif; ?>">
-        <div class="swiper-wrapper">
-            <?php foreach($arResult['BANNERS']['ADDITIONAL'] as $banner): ?>
-              <div class="swiper-slide header-banner-item" data-swiper-autoplay="<?=$banner['autoplay']?>">
-                  <?php if ($banner['link']): ?>
-                    <a href="<?=$banner['link']?>" target="<?=$banner['target']?>"><img src="<?=$banner['picture']['src']?>" class="header-banner-item__picture" alt="" /></a>
-                  <?php else: ?>
-                    <img src="<?=$banner['picture']['src']?>" class="header-banner-item__picture" alt="" />
-                  <?php endif; ?>
-              </div>
-            <?php endforeach; ?>
+              <?php if(count($arResult['BANNERS']['ADDITIONAL']) > 1): ?>
+                <div class="header-banners-nav header-banners-nav__prev">
+                  <div class="header-banners-nav__icon"></div>
+                </div>
+                <div class="header-banners-nav header-banners-nav__next">
+                  <div class="header-banners-nav__icon"></div>
+                </div>
+              <?php endif; ?>
+          </div>
         </div>
-          <?php if(count($arResult['BANNERS']['ADDITIONAL']) > 1): ?>
-            <div class="header-banners-nav header-banners-nav__prev">
-              <div class="header-banners-nav__icon"></div>
-            </div>
-            <div class="header-banners-nav header-banners-nav__next">
-              <div class="header-banners-nav__icon"></div>
-            </div>
-          <?php endif; ?>
-      </div>
-    </div>
+      <?php endif; ?>
   </div>
 </div>
