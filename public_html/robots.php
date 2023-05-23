7c636fc6f1a95897720e871675742055170f63ee
fidget <fidget@fidget.ru>
2023-05-24 01:19:07 +0300

    feature(project): robots.txt для мультидоменов (#10)

--- public_html/robots.php
+++ public_html/robots.php
@@ -1,30 +1,95 @@
-<?
-$arHost = explode( ":", $_SERVER["HTTP_HOST"]);
-$_SERVER["HTTP_HOST"] = $arHost[0];
+<?php
+/** @var array $_SERVER */
+
 $hostname = $_SERVER['HTTP_HOST'];
+$file = $_SERVER["DOCUMENT_ROOT"] . '/robots.php';
+$filemtime = filemtime($file);
 
-function echoTextFile($file) {
-	if (! file_exists($file)) return false;
-	if (! is_readable($file)) return false;
+header('Content-Type: text/plain');
+header('Content-Length: '.filesize($file));
+header('Last-Modified: ' . gmdate('D, d M Y H:i:s ', $filemtime) . 'GMT');
+header('ETag: ' . md5($file . $filemtime));
+?>
+User-Agent: *
+Disallow: */index.php
+Disallow: /bitrix/
+Disallow: /basket/
+Disallow: */filter/*
+Disallow: /*show_include_exec_time=
+Disallow: /*show_page_exec_time=
+Disallow: /*show_sql_stat=
+Disallow: /*bitrix_include_areas=
+Disallow: /*clear_cache=
+Disallow: /*clear_cache_session=
+Disallow: /*ADD_TO_COMPARE_LIST
+Disallow: /*ORDER_BY
+Disallow: /*PAGEN
+Disallow: /*?print=
+Disallow: /*&print=
+Disallow: /*print_course=
+Disallow: /*?action=
+Disallow: /*&action=
+Disallow: /*register=
+Disallow: /*forgot_password=
+Disallow: /*change_password=
+Disallow: /*login=
+Disallow: /*logout=
+Disallow: /*auth=
+Disallow: /*backurl=
+Disallow: /*back_url=
+Disallow: /*BACKURL=
+Disallow: /*BACK_URL=
+Disallow: /*back_url_admin=
+Disallow: /*?utm_source=
+Disallow: /*?bxajaxid=
+Disallow: /*RID=false
+Disallow: /*quiz=yes
+Disallow: /*?q=
+Allow: /bitrix/components/
+Allow: /bitrix/cache/
+Allow: /bitrix/js/
+Allow: /bitrix/templates/
+Allow: /bitrix/panel/
+Sitemap: https://<?=$hostname?>/sitemap.xml
 
-	$timestamp = filemtime($file);
-	$tsstring = gmdate('D, d M Y H:i:s ', $timestamp) . 'GMT';
-	$etag = md5($file . $timestamp);
-
-	header('Content-Type: text/plain');
-	header('Content-Length: '.filesize($file));
-	header("Last-Modified: $tsstring");
-    header("ETag: \"{$etag}\"");
-
-	readfile($file);
-
-	return true;
-}
-
-$robotsHost = dirname(__FILE__) . "/aspro_regions/robots/robots_{$hostname}.txt";
-$robotsDefault = dirname(__FILE__) . "/robots.txt";
-
-if(!echoTextFile($robotsHost) && !echoTextFile($robotsDefault)) 
-{
-	header('HTTP/1.0 404 Not Found');
-}
+User-Agent: Yandex
+Disallow: */index.php
+Disallow: /bitrix/
+Disallow: /basket/
+Disallow: */filter/*
+Disallow: /*show_include_exec_time=
+Disallow: /*show_page_exec_time=
+Disallow: /*show_sql_stat=
+Disallow: /*bitrix_include_areas=
+Disallow: /*clear_cache=
+Disallow: /*clear_cache_session=
+Disallow: /*ADD_TO_COMPARE_LIST
+Disallow: /*ORDER_BY
+Disallow: /*PAGEN
+Disallow: /*?print=
+Disallow: /*&print=
+Disallow: /*print_course=
+Disallow: /*?action=
+Disallow: /*&action=
+Disallow: /*register=
+Disallow: /*forgot_password=
+Disallow: /*change_password=
+Disallow: /*login=
+Disallow: /*logout=
+Disallow: /*auth=
+Disallow: /*backurl=
+Disallow: /*back_url=
+Disallow: /*BACKURL=
+Disallow: /*BACK_URL=
+Disallow: /*back_url_admin=
+Disallow: /*?utm_source=
+Disallow: /*?bxajaxid=
+Disallow: /*RID=false
+Disallow: /*quiz=yes
+Disallow: /*?q=
+Allow: /bitrix/components/
+Allow: /bitrix/cache/
+Allow: /bitrix/js/
+Allow: /bitrix/templates/
+Allow: /bitrix/panel/
+Host: https://<?=$hostname?>/
