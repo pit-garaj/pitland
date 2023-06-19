ef7769df426d762f07c4bf1f7678f4b7024c0454
fidget <fidget@fidget.ru>
2023-06-19 23:58:19 +0300

    feature(project): Умный поиск (#35)

--- public_html/include/top_page/search.title.catalog.php
+++ public_html/include/top_page/search.title.catalog.php
@@ -1,46 +1,50 @@
-<?$APPLICATION->IncludeComponent(
-	"bitrix:search.title", 
-	"corp", 
-	array(
-		"CATEGORY_0" => array(
-			0 => "iblock_aspro_next_catalog",
+<div class="search-wrapper">
+	<?$APPLICATION->IncludeComponent(
+		//"bitrix:search.title",
+		//"corp",
+		"arturgolubev:search.title",
+		".default",
+		array(
+			"CATEGORY_0" => array(
+				0 => "iblock_aspro_next_catalog",
+			),
+			"CATEGORY_0_TITLE" => "Товары",
+			"CATEGORY_0_iblock_aspro_next_catalog" => array(
+				0 => "23",
+			),
+			"CATEGORY_0_iblock_aspro_next_content" => array(
+				0 => "all",
+			),
+			"CATEGORY_OTHERS_TITLE" => "Еще",
+			"CHECK_DATES" => "Y",
+			"COMPONENT_TEMPLATE" => "corp",
+			"COMPOSITE_FRAME_MODE" => "N",
+			"COMPOSITE_FRAME_TYPE" => "AUTO",
+			"CONTAINER_ID" => "title-search_fixed",
+			"CONVERT_CURRENCY" => "Y",
+			"CURRENCY_ID" => "RUB",
+			"INPUT_ID" => "title-search-input_fixed",
+			"NUM_CATEGORIES" => "1",
+			"ORDER" => "rank",
+			"PAGE" => CNext::GetFrontParametrValue("CATALOG_PAGE_URL"),
+			"PREVIEW_HEIGHT" => "38",
+			"PREVIEW_TRUNCATE_LEN" => "50",
+			"PREVIEW_WIDTH" => "38",
+			"PRICE_CODE" => array(
+				0 => "Основной тип цен продажи",
+			),
+			"PRICE_VAT_INCLUDE" => "Y",
+			"SHOW_ANOUNCE" => "N",
+			"SHOW_INPUT" => "Y",
+			"SHOW_INPUT_FIXED" => "Y",
+			"SHOW_OTHERS" => "N",
+			"SHOW_PREVIEW" => "Y",
+			"TOP_COUNT" => "999999",
+			"USE_LANGUAGE_GUESS" => "N"
 		),
-		"CATEGORY_0_TITLE" => "Товары",
-		"CATEGORY_0_iblock_aspro_next_catalog" => array(
-			0 => "23",
-		),
-		"CATEGORY_0_iblock_aspro_next_content" => array(
-			0 => "all",
-		),
-		"CATEGORY_OTHERS_TITLE" => "Еще",
-		"CHECK_DATES" => "Y",
-		"COMPONENT_TEMPLATE" => "corp",
-		"COMPOSITE_FRAME_MODE" => "N",
-		"COMPOSITE_FRAME_TYPE" => "AUTO",
-		"CONTAINER_ID" => "title-search_fixed",
-		"CONVERT_CURRENCY" => "Y",
-		"CURRENCY_ID" => "RUB",
-		"INPUT_ID" => "title-search-input_fixed",
-		"NUM_CATEGORIES" => "1",
-		"ORDER" => "rank",
-		"PAGE" => CNext::GetFrontParametrValue("CATALOG_PAGE_URL"),
-		"PREVIEW_HEIGHT" => "38",
-		"PREVIEW_TRUNCATE_LEN" => "50",
-		"PREVIEW_WIDTH" => "38",
-		"PRICE_CODE" => array(
-			0 => "Основной тип цен продажи",
-		),
-		"PRICE_VAT_INCLUDE" => "Y",
-		"SHOW_ANOUNCE" => "N",
-		"SHOW_INPUT" => "Y",
-		"SHOW_INPUT_FIXED" => "Y",
-		"SHOW_OTHERS" => "N",
-		"SHOW_PREVIEW" => "Y",
-		"TOP_COUNT" => "999999",
-		"USE_LANGUAGE_GUESS" => "N"
-	),
-	false,
-	array(
-		"ACTIVE_COMPONENT" => "Y"
-	)
-);?>
+		false,
+		array(
+			"ACTIVE_COMPONENT" => "Y"
+		)
+	);?>
+</div>
