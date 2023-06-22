9a360049aa2eb3dd2164263d0ec97727c56793c8
fidget <fidget@fidget.ru>
2023-06-22 11:46:56 +0300

    feature(project): Исправляет бесплатная на быстрая доставка по России

--- public_html/bitrix/templates/aspro_next/components/bitrix/catalog.element/main7/blocks/features_tehnika.php
+++ public_html/bitrix/templates/aspro_next/components/bitrix/catalog.element/main7/blocks/features_tehnika.php
@@ -1,5 +1,5 @@
 <div class="catItemsFeatures">
-    <div class="catItemsFeatures__item icon delivery"><a href="/help/delivery/">Бесплатная доставка по России</a></div>
+    <div class="catItemsFeatures__item icon delivery"><a href="/help/delivery/">Быстрая доставка по России</a></div>
     <div class="catItemsFeatures__item icon guarantee"><a href="/help/warranty/">Гарантия на товар</a></div>
     <div class="catItemsFeatures__item icon repair"><a href="" data-event="jqm" data-param-form_id="ZAPIS" data-name="cheaper">Ремонт техники</a></div>
     <div class="catItemsFeatures__item icon quality"><a href="/features/quality/">Качественная сборка</a></div>
