d5c9b3e6977cc0c2cee7f424dd5965876bac7571
fidget <fidget@fidget.ru>
2023-07-02 14:57:07 +0300

    feature(project): Заказать звонок (#44)

--- public_html/ajax/form.php
+++ public_html/ajax/form.php
@@ -76,6 +76,43 @@ $url_sizes = htmlspecialchars($_REQUEST['url']) ?: '';
                 ]
             ]
         );
+    } else if ($_REQUEST["form_id"] === 'CALLBACK_FOOTER') {
+        $APPLICATION->IncludeComponent(
+            "bitrix:form",
+            "popup_callback",
+            [
+                "AJAX_MODE"              => "Y",
+                "SEF_MODE"               => "N",
+                "WEB_FORM_ID"            => $form_id,
+                "START_PAGE"             => "new",
+                "SHOW_LIST_PAGE"         => "N",
+                "SHOW_EDIT_PAGE"         => "N",
+                "SHOW_VIEW_PAGE"         => "N",
+                "SUCCESS_URL"            => "",
+                "SHOW_ANSWER_VALUE"      => "N",
+                "SHOW_ADDITIONAL"        => "N",
+                "SHOW_STATUS"            => "N",
+                "EDIT_ADDITIONAL"        => "N",
+                "EDIT_STATUS"            => "Y",
+                "NOT_SHOW_FILTER"        => "",
+                "NOT_SHOW_TABLE"         => "",
+                "CHAIN_ITEM_TEXT"        => "",
+                "CHAIN_ITEM_LINK"        => "",
+                "IGNORE_CUSTOM_TEMPLATE" => "N",
+                "USE_EXTENDED_ERRORS"    => "Y",
+                "CACHE_GROUPS"           => "N",
+                "CACHE_TYPE"             => "A",
+                "CACHE_TIME"             => "3600000",
+                "AJAX_OPTION_JUMP"       => "N",
+                "AJAX_OPTION_STYLE"      => "Y",
+                "AJAX_OPTION_HISTORY"    => "N",
+                "SHOW_LICENCE"           => CNext::GetFrontParametrValue('SHOW_LICENCE'),
+                "HIDDEN_CAPTCHA"         => CNext::GetFrontParametrValue('HIDDEN_CAPTCHA'),
+                "VARIABLE_ALIASES"       => [
+                    "action" => "action"
+                ]
+            ]
+        );
     } else {
         $APPLICATION->IncludeComponent(
             "bitrix:form",
--- public_html/local/php_interface/init.php
+++ public_html/local/php_interface/init.php
@@ -14,3 +14,19 @@ $checkCity = Cities::checkCity();
 if ($checkCity !== 'city' && Cities::isSubDomain()) {
     LocalRedirect('http://' . SITE_SERVER_NAME);
 }
+
+
+
+/*почтовое событие сделать, чтобы поле время отправлялось в виде 07:30, а не 07:30[112]*/
+AddEventHandler("main", "OnBeforeEventAdd", array("MyClass", "OnBeforeEventAddHandler"));
+class MyClass
+{
+    function OnBeforeEventAddHandler(&$event, &$lid, &$arFields)
+    {
+        if ($event == 'FORM_FILLING_CALLBACK_FOOTER'){
+            $pos = strpos($arFields["TIME"], '[');
+            $str = substr($arFields["TIME"], 0, $pos);
+            $arFields["TIME_CUSTOM"] = $str;
+        }
+    }
+}
