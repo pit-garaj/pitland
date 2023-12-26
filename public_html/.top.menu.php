ce1f6988e5193424789ebfa2dda3331607e738c5
Fidget <fidget@fidget.ru>
2023-12-26 12:32:31 +0300

    feature(project): Исправление top menu

--- public_html/.top.menu.php
+++ public_html/.top.menu.php
@@ -1,18 +1,12 @@
 <?php
 
-use Ninja\Project\Regionality\Cities;
-
-$city = Cities::getCityByHost();
-
-$aMenuLinks[] = ['О компании', '/company/', [], [], ''];
-
-if (!empty($city['default'])) {
-    $aMenuLinks[] = ['Услуги', '/services/', [], [], ''];
-}
-
-$aMenuLinks[] = ['Как купить', '/help/', [], [], ''];
-$aMenuLinks[] = ['Производители', '/info/brands/', [], [], ''];
-$aMenuLinks[] = ['Оплата', '/help/payment/', [], [], ''];
-$aMenuLinks[] = ['Доставка', '/help/delivery/', [], [], ''];
-$aMenuLinks[] = ['Акции', '/sale/', [], [], ''];
-$aMenuLinks[] = ['Контакты', '/contacts/', [], [], ''];
+$aMenuLinks = [
+  ['О компании', '/company/', [], [], ''],
+  ['Услуги', '/services/', [], [], ''],
+  ['Как купить', '/help/', [], [], ''],
+  ['Производители', '/info/brands/', [], [], ''],
+  ['Оплата', '/help/payment/', [], [], ''],
+  ['Доставка', '/help/delivery/', [], [], ''],
+  ['Акции', '/sale/', [], [], ''],
+  ['Контакты', '/contacts/', [], [], ''],
+];
--- public_html/include/menu/menu.topest.php
+++ public_html/include/menu/menu.topest.php
@@ -3,6 +3,13 @@ global $arTheme;
 
 /** @var object $APPLICATION */
 
+use Ninja\Project\Regionality\Cities;
+
+$city = Cities::getCityByHost();
+
+
+$rootMenuType = !empty($city['default']) ? 'top' : 'top-regional';
+
 $APPLICATION->IncludeComponent('bitrix:menu', 'top_content_row', [
     'ALLOW_MULTI_SELECT' => 'N',
     'CHILD_MENU_TYPE' => 'left',
@@ -15,6 +22,6 @@ $APPLICATION->IncludeComponent('bitrix:menu', 'top_content_row', [
     'MENU_CACHE_TYPE' => 'N',
     'MENU_CACHE_USE_GROUPS' => 'N',
     'CACHE_SELECTED_ITEMS' => 'N',
-    'ROOT_MENU_TYPE' => 'top',
+    'ROOT_MENU_TYPE' => $rootMenuType,
     'USE_EXT' => 'Y'
 ]);
